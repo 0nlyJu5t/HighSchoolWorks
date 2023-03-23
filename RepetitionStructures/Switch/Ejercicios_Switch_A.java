556a1640a6a7d2a9ccebd7c345f89e4f46849685
JustinDavid <122066921+0nlyJu5t@users.noreply.github.com>
2023-03-23 07:47:56 -0600

    Create Ejercicios_Switch_A.java

---
+++ RepetitionStructures/Switch/Ejercicios_Switch_A.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_Switch;
+
+/**
+ *
+ * @author Justin David Damito Sanchez
+ */
+import java.util.Scanner;
+public class Ejercicios_Switch_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       
+        //inicializacion
+        int num, num1,option,rest;
+        double restD;
+        
+        System.out.println("ingrese un numeros: ");//peticion de datos 
+        num = sc.nextInt();//entrada
+        System.out.println("ingrese otro numeros: ");//peticion de datos
+        num1 = sc.nextInt();//entrada
+        
+        System.out.println("que operacion desea realizar:\n1.suma\n2.resta\n3.multiplicacion\n4.division\n5.promedio ");//peticion de datos
+        option = sc.nextInt();
+        
+        //switch oprion
+        switch(option){
+            case 1:
+                rest = num + num1;//operacion
+                System.out.println("el resultado de la suma de " + num + " y " + num1 + " es de: " + rest);//salida
+                break;
+            case 2:
+                rest = num - num1;//operacion
+                System.out.println("el resultado de la resta " + num + " y " + num1 + " es de: " + rest);//salida
+                break;
+            case 3:
+                rest = num * num1;//operacion
+                System.out.println("el resultado de la multiplicacion " + num + " y " + num1 + " es de: " + rest);//salida
+                break;
+            case 4:
+                restD = num / num1;//operacion
+                System.out.println("el resultado de la division " + num + " y " + num1 + " es de: " + restD);//salida
+                break;
+            case 5:
+                rest = (num + num1)/2;//operacion
+                System.out.println("el promedio " + num + " y " + num1 + " es de: " + rest);//salida
+            default:
+                System.out.println("undefined");//salida
+
+        }        
+    }
+}
