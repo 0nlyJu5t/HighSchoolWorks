1c1f61ececab7ac87e35cccfa05eab4096b0b578
JustinDavid <122066921+0nlyJu5t@users.noreply.github.com>
2023-03-23 07:46:37 -0600

    Create Ejercicios_For_A.java

---
+++ RepetitionStructures/For/Ejercicios_For_A.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_For;
+
+/**
+ *
+ * @author Justin David Damito Sanchez
+ */
+public class Ejercicios_For_A {
+    public static void main(String[] args) {
+        
+        //bucle
+        for(int i = 5;i <= 30;i++){//condicones del for
+            if(i % 2 == 0){//si el resto de 1 entre 2 es 0
+                System.out.println(i);}//salida
+        }
+    }
+}
