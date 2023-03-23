8223141ec54b673826c09d472547a68de9ecff98
JustinDavid <122066921+0nlyJu5t@users.noreply.github.com>
2023-03-23 07:38:04 -0600

    Add files via upload

---
+++ Variables/datos.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica01;
+
+/**
+ *
+ * @author Justin David Damito Sanchez 
+ */
+public class datos {
+    public static void main(String[] args) {
+        //tipos de datos
+        //enteros(byte, short , int, long)
+        /*byte variableBytes = 10;
+        System.out.println("Byte------------------------------------------");
+        System.out.println("tamaño de un tipo tipo byte "+Byte.SIZE);
+        System.out.println("valor maximo de un tipo byte "+Byte.MAX_VALUE);
+        System.out.println("valor minimo de un tipo byte "+Byte.MIN_VALUE);
+        
+        short variableShort = 1020;
+        System.out.println("SHORT-----------------------------------------");
+        System.out.println("cantidad de bits de un tipo short "+Short.BYTES);
+        System.out.println("tamaño de un tipo Short "+Short.SIZE);
+        System.out.println("valor maximo de un tipo Short "+Short.MAX_VALUE);
+        System.out.println("valor minimo de un tipo Short "+Short.MIN_VALUE);
+        
+        int variableInt = 20;
+        System.out.println("INT------------------------------------------");
+        System.out.println("cantidad de bits de un tipo Int "+Integer.BYTES);
+        System.out.println("tamaño de un tipo Int "+Integer.SIZE);
+        System.out.println("valor maximo de un tipo Int "+Integer.MAX_VALUE);
+        System.out.println("valor minimo de un tipo Int "+Integer.MIN_VALUE);
+        
+        long variableLong = 2000;
+        System.out.println("LONG------------------------------------------");
+        System.out.println("cantidad de bits de un tipo Long "+Long.BYTES);
+        System.out.println("tamaño de un tipo Long "+Long.SIZE);
+        System.out.println("valor maximo de un tipo Long "+Long.MAX_VALUE);
+        System.out.println("valor minimo de un tipo Long "+Long.MIN_VALUE);
+        
+        System.out.println("");
+        //utilizar numeros binarios/octales/hexadecimales
+        //decimales
+
+        int nDecimal = 10;
+        System.out.println("el valor de el numero decimal es de: " + nDecimal);
+        
+        //octales 
+        int nOctales = 012;
+        System.out.println("el valor de el numro octal es de: "+ nOctales);
+    
+        //hexadecimal 
+        int nHexa = 0xA;
+        System.out.println("el valor de el numero hexa es de: "+ nHexa);
+        
+        int nBinario = 0b1010;
+        System.out.println("el valor de el numero benario es de: "+ nBinario);*/
+        
+        //tipos primitivos flotantes float/double
+        //float
+        
+        float variableFloat = 1000.10f;
+        System.out.println("el valor de el float es de: " +variableFloat );
+        System.out.println("cantidad de bits de un tipo float "+Float.BYTES);
+        System.out.println("tamaño de un Float "+Float.SIZE);
+        System.out.println("valor maximo de un tipo Float "+Float.MAX_VALUE);
+        System.out.println("valor minimo de un tipo Float "+Float.MIN_VALUE);
+        
+        double variableDouble = 1000.10;
+        System.out.println("el valor de el double es de "+variableDouble);
+        System.out.println("cantidad de bits de un tipo double "+Double.BYTES);
+        System.out.println("tamaño de un Double "+Double.SIZE);
+        System.out.println("valor maximo de un tipo Float "+Float.MAX_VALUE);
+        System.out.println("valor minimo de un tipo Float "+Float.MIN_VALUE);
+        
+        //tipo primitivo char
+        char vChar = 'A';
+        System.out.println("el valor de el char es de "+vChar);
+        System.out.println("cantidad de bits de un tipo char "+Character.BYTES);
+        System.out.println("tamaño de un char "+Character.SIZE);
+        System.out.println("valor maximo de un tipo vhar "+Character.MAX_VALUE);
+        System.out.println("valor minimo de un tipo vhar "+Character.MIN_VALUE);
+        
+        //codigo uni
+        char vChar2 = '\u0041';
+        System.out.println("el valor de el char2 es de "+vChar2);
+         
+        //signos 
+        char vChar3 = '!';
+        System.out.println("el valor de el char3 es de "+vChar3);
+         
+        //numeros 
+        char vChar4 = 33;
+        System.out.println("el valor de el char4 es de "+vChar4);
+        
+        boolean vboolean = true;
+        System.out.println("el valor de vboolean es "+vboolean);
+        System.out.println("valor de verdad tipo booelan "+Boolean.TRUE);
+        System.out.println("valor de verdad tipo booelan "+Boolean.FALSE);
+    }
+}
