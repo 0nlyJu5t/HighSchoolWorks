b403e8ba2cce98436d42cf426a4ca8cfb9b7a509
JustinDavid <122066921+0nlyJu5t@users.noreply.github.com>
2023-03-23 07:43:17 -0600

    Create Ejercicios_While_A.java

---
+++ RepetitionStructures/while/Ejercicios_While_A.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_While;
+
+/**
+ *
+ * @author Justin David Damito Sanchez
+ */
+public class Ejercicios_While_A {
+    public static void main(String[] args) {
+        
+        //inicializacion
+        int i = 60;
+        //bucle
+        while(i >= 45){//mientras que i sea menor o igual a 45
+            System.out.println(i);//salida
+            i--;//decremento
+        }
+    }
+}
